c3355b80869944afd105b96584ddda8f3eb743d5
Ivang71 <artemtolv@gmail.com>
2023-12-14 17:22:39 +0300

    fix displaying

--- src/scripts/rendering/renderManager.ts
+++ src/scripts/rendering/renderManager.ts
@@ -44,18 +44,55 @@ class RenderManager {
         const baseMatrix = mat3.create()
         mat3.projection(baseMatrix, canvas.clientWidth, canvas.clientHeight)
 
+        let totalSize = 0
         for (const entity of this.renderQueue) {
-            gl.bufferData(gl.ARRAY_BUFFER, entity.vertices, gl.DYNAMIC_DRAW)
+            totalSize += entity.vertices.length * Float32Array.BYTES_PER_ELEMENT
+        }
+
+        const buffer = gl.createBuffer()
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
+        gl.bufferData(gl.ARRAY_BUFFER, totalSize * Float32Array.BYTES_PER_ELEMENT, gl.DYNAMIC_DRAW)
+        let offset = 0
+
+        for (const entity of this.renderQueue) {
+            gl.bufferSubData(gl.ARRAY_BUFFER, offset, new Float32Array(entity.vertices))
             gl.uniform4f(this.colorLoc, ...entity.color)
-            
+
             const matrix = mat3.clone(baseMatrix)
             mat3.translate(matrix, matrix, entity.position)
             mat3.rotate(matrix, matrix, entity.rotation)
             mat3.scale(matrix, matrix, entity.size)
 
             gl.uniformMatrix3fv(this.matrixLoc, false, matrix)
-            gl.drawArrays(gl.TRIANGLES, 0, entity.vertices.length)
+            gl.drawArrays(gl.TRIANGLES, offset, entity.vertices.length)
+            offset += entity.vertices.length * Float32Array.BYTES_PER_ELEMENT
         }
+
+        // const squareVertices = new Float32Array([
+        //     -0.5, -0.5,   // Bottom-left corner
+        //      0.5, -0.5,   // Bottom-right corner
+        //      0.5,  0.5,   // Top-right corner
+        //     -0.5,  0.5,   // Top-left corner
+        //   ]);
+      
+        //   gl.bindBuffer(gl.ARRAY_BUFFER, this.positionBuffer);
+        //   gl.bufferData(gl.ARRAY_BUFFER, squareVertices, gl.STATIC_DRAW);
+      
+        //   gl.enableVertexAttribArray(this.positionAttribLoc);
+        //   gl.vertexAttribPointer(this.positionAttribLoc, 2, gl.FLOAT, false, 0, 0);
+      
+        //   // Set a simple matrix (identity matrix in this case)
+        //   const matrix = mat3.create();
+      
+        //   // You can add additional transformations if needed
+      
+        //   gl.uniformMatrix3fv(this.matrixLoc, false, matrix);
+      
+        //   // Set a color (e.g., white)
+        //   gl.uniform4f(this.colorLoc, 1.0, 1.0, 1.0, 1.0);
+      
+        //   // Draw the square
+        //   gl.drawArrays(gl.TRIANGLE_FAN, 0, 4)
 
         this.renderQueue = []
     }
